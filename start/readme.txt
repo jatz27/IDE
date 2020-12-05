6cf6240e02ca116f1e89e3b64581a9ced1ba2bb9
jatzWSL <julioatz90@gmail.com>
2020-12-05 01:05:11 -0600

    Se agrego le directorio start

---
+++ start/readme.txt
@@ -0,0 +1,2 @@
+1. ejecutar primero vim-plug.sh -> instalara vim.plug el manejador de plugins.
+2. ejecutar start.sh -> intalara y configurara todas las dependencias necesarias.
--- start/start.sh
+++ start/start.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/bash
-# --Plugins-------------------------------------------------------------
-# Se instala vim plug
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-mkdir ~/.local/share/nvim/plugged # Se crea la carpeta donde se guardaran los plugins 
-# ----------------------------------------------------------------------
 #----C-O-C----------------------------------------------------------------
 set -o nounset    # error when referencing undefined variable
 set -o errexit    # exit when command fails
